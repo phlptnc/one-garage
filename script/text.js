2f8666946f1b92b455110cc72044fa241746d94b
Philip Joseph <139831746+phlptnc@users.noreply.github.com>
2023-10-04 17:01:06 +0800

    Media Query
    Added menu responsiveness and script
    
    Added media query and still ongoing..
    
    Responsive design still around 50%

--- script/text.js
+++ script/text.js
@@ -2,3 +2,17 @@ window.addEventListener('scroll', function (){
     const header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 0);
 });
+
+
+// menu-button
+const menuBtn = document.querySelector('#menu-button');
+const menuBox = document.querySelector('#nav-bar');
+const closeBtn = document.querySelector('#close-button');
+
+menuBtn.addEventListener('click', () => {
+    menuBox.style.display = 'flex';
+});
+
+closeBtn.addEventListener('click', () => {
+    menuBox.style.display = 'none';
+})
